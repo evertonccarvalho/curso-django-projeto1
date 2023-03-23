58b0375dd145930a5d6b14e4ef1520f22ea7676f
evertonsnake <evertonsnake@gmail.com>
2023-03-23 15:01:47 -0300

    removido contato e sobre

--- projeto/settings.py
+++ projeto/settings.py
@@ -55,11 +55,7 @@ ROOT_URLCONF = 'projeto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            BASE_DIR / 'base_template',
-            BASE_DIR / 'recipes' / 'templates_temp'
-
-        ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
